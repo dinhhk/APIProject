11d0c2b8103f088123f4e345972e24ebd3e4c0b7
Dinh Han <hankimdinh@gmail.com>
2017-04-16 10:32:56 +0700

    [API-2] Require users controller in the users routes

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const UsersController = require('../controllers/users');
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({
-    'message': 'You requested index page'
-  });
-});
+router.route('/')
+  .get(UsersController.index);
 
 module.exports = router;
