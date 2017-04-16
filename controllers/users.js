a654c6b6d28e30705d12c61ed0778ede639ee040
Dinh Han <hankimdinh@gmail.com>
2017-04-16 12:57:35 +0700

    [API-3] Create a user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,21 @@
+const User = require('../models/user');
+
 module.exports = {
   index: (req, res, next) => {
-    res.status(200).json({
-      'message': 'You requested index page'
+    User.find({}, (err, user) => {
+      if(err) {
+        next(err);
+      }
+      res.status(200).json(user);
     });
+  },
+  newUser: (req, res, next) => {
+    const newUser = new User(req.body);
+    newUser.save((err, user) => {
+      if(err) {
+        next(err);
+      }
+      res.status(201).json(user);
+    })
   }
 }
